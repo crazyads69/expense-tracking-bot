ed2036f85817c718e73b03881d3a955cb67b9266
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-11-26 23:13:33 +0700

    first commit

---
+++ models/expense.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const ExpenseSchema = z.object({
+  id: z.string(),
+  amount: z.number().positive(),
+  category: z.enum([
+    "Ăn uống",
+    "Di chuyển",
+    "Tiện ích",
+    "Giải trí",
+    "Mua sắm",
+    "Sức khỏe",
+    "Học tập",
+    "Nhà cửa",
+    "Khác",
+  ]),
+  description: z.string(),
+  date: z.date().default(() => new Date()),
+  paymentMethod: z
+    .enum(["Tiền mặt", "Thẻ ngân hàng", "Ví điện tử", "Khác"])
+    .default("Tiền mặt"),
+});
+
+export type Expense = z.infer<typeof ExpenseSchema>;
